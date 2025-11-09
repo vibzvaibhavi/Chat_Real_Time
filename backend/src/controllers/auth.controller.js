d03f8fbef1889c5206a980d3fdb54cb5313fe4d8
vibzvaibhavi <vibzvaibhavi@gmail.com>
2025-11-08 21:11:21 -0500

    signup code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs"
 export const signup = async (req,res) => {
@@ -32,8 +33,11 @@ export const signup = async (req,res) => {
       })
 
       if(newUser) {
-        generateToken(newUser._id, res)
-        await newUser.save()
+        //generateToken(newUser._id, res)
+        //await newUser.save()
+
+        const savedUser = await newUser.save();
+        generateToken(savedUser._id, res);
         res.status(201).json({
             _id:newUser._id,
             fullName:newUser.fullName,
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
     try {
+        const {MONGO_URI} = process.env;
+        if (!MONGO_URI) throw new Error("Mongo_URI is not sset");
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log("MONGO CONNECTED", conn.connection.host)
     }  catch (error) {
